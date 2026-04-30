c979804170325b368705ef495d7e9e87ff3e7c5d
Alexander Tu <antu2@uci.edu>
2026-04-29 19:51:51 -0700

    Fixed safety checks for None

--- scraper.py
+++ scraper.py
@@ -71,7 +71,7 @@ def is_valid(url: str) -> bool:
 
 
 def record_link_information(url: str, resp) -> None:
-    if resp is None or resp.raw_content is None or resp.raw_content.content is None:
+    if resp is None or resp.raw_response is None or resp.raw_response.content is None:
         return
     parsed_info_iter = statistics_helpers.parse_response(url, resp)
     statistics_helpers.write_count(resp, parsed_info_iter)
